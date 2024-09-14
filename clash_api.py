82f71c91db081d37a57f818768c30f4a4064e032
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-09-14 17:06:37 -0400

    Full Cleaning and Chart of Past War

--- clash_api.py
+++ clash_api.py
@@ -1,7 +1,42 @@
 import os
 from dotenv import load_dotenv
+import requests
+import pandas as pd
+import json
 
 load_dotenv
 
 token = os.getenv("API_TOKEN")
 
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application.json'
+}
+response = requests.get('https://api.clashofclans.com/v1/clans/%232GLLPQYLL/currentwar', headers=headers)
+response = response.json()
+print(json.dumps(response, indent=2))
+
+
+# df = pd.json_normalize(response, 
+#                        record_path=['clan', 'members'], 
+#                        meta=[
+#                            'state', 
+#                            'teamSize', 
+#                            'attacksPerMember', 
+#                            'battleModifier', 
+#                            'preparationStartTime', 
+#                            'startTime', 
+#                            'endTime', 
+#                            ['clan', 'tag'], 
+#                            ['clan', 'name'], 
+#                            ['clan', 'badgeUrls', 'small'], 
+#                            ['clan', 'badgeUrls', 'large'], 
+#                            ['clan', 'badgeUrls', 'medium'], 
+#                            ['clan', 'clanLevel'], 
+#                            ['clan', 'attacks'], 
+#                            ['clan', 'stars'], 
+#                            ['clan', 'destructionPercentage']
+#                        ], 
+#                        record_prefix='member_')
+
+# print(df)
