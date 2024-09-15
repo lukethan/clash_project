86f085957446d756d87c05c185cb995d2fe1a816
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-09-15 13:32:38 -0400

    New API Requests

--- clash_api.py
+++ clash_api.py
@@ -3,40 +3,26 @@ from dotenv import load_dotenv
 import requests
 import pandas as pd
 import json
+import csv
 
 load_dotenv
 
 token = os.getenv("API_TOKEN")
+tag = os.getenv("TAG")
 
+url = f"https://api.clashofclans.com/v1/players/{tag}"
 headers = {
     'Authorization': f'Bearer {token}',
     'Content-Type': 'application.json'
 }
-response = requests.get('https://api.clashofclans.com/v1/clans/%232GLLPQYLL/currentwar', headers=headers)
-response = response.json()
-print(json.dumps(response, indent=2))
+
+response = requests.get(url, headers=headers)
+# print(type(response))
+response_json = response.json()
+new = (json.dumps(response_json, indent=4))
+print(new)
 
 
-# df = pd.json_normalize(response, 
-#                        record_path=['clan', 'members'], 
-#                        meta=[
-#                            'state', 
-#                            'teamSize', 
-#                            'attacksPerMember', 
-#                            'battleModifier', 
-#                            'preparationStartTime', 
-#                            'startTime', 
-#                            'endTime', 
-#                            ['clan', 'tag'], 
-#                            ['clan', 'name'], 
-#                            ['clan', 'badgeUrls', 'small'], 
-#                            ['clan', 'badgeUrls', 'large'], 
-#                            ['clan', 'badgeUrls', 'medium'], 
-#                            ['clan', 'clanLevel'], 
-#                            ['clan', 'attacks'], 
-#                            ['clan', 'stars'], 
-#                            ['clan', 'destructionPercentage']
-#                        ], 
-#                        record_prefix='member_')
-
-# print(df)
+# with open('player.csv', 'a', newline="") as file:
+#     write = csv.writer(file)
+#     file.write(new)
