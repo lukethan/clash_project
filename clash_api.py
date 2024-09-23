830ef3ade3a3f82b377654bf859cf576010b0b7a
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-09-23 19:15:16 -0400

    Fixed Cron

--- clash_api.py
+++ clash_api.py
@@ -3,22 +3,24 @@ from dotenv import load_dotenv
 import requests
 import json
 
-load_dotenv
+load_dotenv()
 
 token = os.getenv("API_TOKEN")
+print(token)
 tag = os.getenv("TAG")
+print(tag)
 
-url = f"https://api.clashofclans.com/v1/players/{tag}"
-headers = {
-    'Authorization': f'Bearer {token}',
-    'Content-Type': 'application.json'
-}
+# url = f"https://api.clashofclans.com/v1/players/{tag}"
+# headers = {
+#     'Authorization': f'Bearer {token}',
+#     'Content-Type': 'application.json'
+# }
 
-response = requests.get(url, headers=headers)
-# print(type(response))
-response_json = response.json()
-new = (json.dumps(response_json, indent=4))
-print(new)
+# response = requests.get(url, headers=headers)
+# # print(type(response))
+# response_json = response.json()
+# new = (json.dumps(response_json, indent=4))
+# print(new)
 
 
 # with open('player.csv', 'a', newline="") as file:
--- data_to_file.py
+++ data_to_file.py
@@ -5,31 +5,48 @@ import json
 from datetime import datetime
 import logging
 
-load_dotenv
+# def main():
+
+log_file_path = '/home/lukethan/projects/clash_project/data_to_file.log'
+
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+load_dotenv('/home/lukethan/projects/clash_project/.env')
 
 token = os.getenv("API_TOKEN")
+if token is None:
+    logging.error('API_TOKEN not found')
 
 headers = {
     'Authorization': f'Bearer {token}',
-    'Content-Type': 'application.json'
+    'Content-Type': 'application/json'
 }
 response = requests.get('https://api.clashofclans.com/v1/clans/%232GLLPQYLL/currentwar', headers=headers)
+logging.info(f"API response status code: {response.status_code}")
 
 response = response.json()
 
-logging.basicConfig(filename='data_to_file.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 dt = datetime.now()
 
-if response['state'] == 'inWar':
-    with open(f'war{dt.month}.{dt.day}.{dt.year}time{dt.hour}.{dt.minute}.json', "a") as json_file:
+directory = '/home/lukethan/projects/clash_project'
+
+# Create the absolute file path
+file_path = os.path.join(directory, f'war{dt.month}.{dt.day}.{dt.year}time{dt.hour}.{dt.minute}.json')
+
+if response["state"] == 'inWar':
+    with open(file_path, "a") as json_file:
         json.dump(response, json_file, indent=4)
-elif response['state'] == 'warEnded':
+        logging.info(f'Response recorded for inWar at {dt}')
+elif response["state"] == 'warEnded':
     end_time = response['endTime'][0:8]
     file_name = f'war{end_time}.json'
-    if os.path.exists(file_name):
-        logging.info(f'{file_name} already exists')
+    file_path = os.path.join(directory, file_name)
+    if os.path.exists(file_path):
+        logging.info(f'{file_path} already exists')
     else:
-        with open(file_name, "a") as json_file:
+        with open(file_path, "a") as json_file:
             json.dump(response, json_file, indent=4)
-        logging.info(f"{file_name} created and response recorded at {dt}")
+        logging.info(f"{file_path} created and response recorded at {dt}")
+
+# if __name__ == "__main__":
+#     main()
